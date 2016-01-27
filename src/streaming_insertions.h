78e69978481f1b0c22f0d2a3e747427338f2069d
Oded Green <ogreen@gatech.edu>
2016-01-27 10:55:02 -0500

    merged deletions with insertions

--- src/streaming_insertions.h
+++ src/streaming_insertions.h
@@ -36,14 +36,18 @@ void moveUpTreeBrandes(bcForest* forest, struct stinger* sStinger,
 //----------------------------------------------
 // Dynamic Graph Deletion
 
-void removeEdgeWithoutMovementBrandes(bcForest* forest, struct stinger* sStinger,
-                            uint64_t currRoot, uint64_t startVertex, uint64_t parentVertex,
-                            uint64_t deletedPathsFromRoot, bc_t* totalBC);
+// void removeEdgeWithoutMovementBrandes(bcForest* forest, struct stinger* sStinger,
+//                             uint64_t currRoot, uint64_t startVertex, uint64_t parentVertex,
+//                             uint64_t deletedPathsFromRoot, bc_t* totalBC);
+void removeEdgeWithoutMovementBrandes(bcForest* forest, struct stinger* sStinger, uint64_t currRoot, 
+                        uint64_t startVertex, uint64_t parentVertex, uint64_t deletedPathsFromRoot, 
+                        extraArraysPerThread *eAPT);
 
 
-void moveDownTreeBrandes(bcForest* forest, struct stinger* sStinger,
-                            uint64_t currRoot, uint64_t startVertex, uint64_t parentVertex, bc_t* totalBC);
-
+// void moveDownTreeBrandes(bcForest* forest, struct stinger* sStinger,
+//                             uint64_t currRoot, uint64_t startVertex, uint64_t parentVertex, bc_t* totalBC);
+void moveDownTreeBrandes(bcForest* forest, struct stinger* sStinger, uint64_t currRoot, 
+                uint64_t startVertex, uint64_t parentVertex, extraArraysPerThread *eAPT);
 
 
 
